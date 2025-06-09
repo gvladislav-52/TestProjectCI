e95d82c517e26b9f5f9a2f6b06a382d294dd3486
gvladislav-52 <gvladislav_52@mail.ru>
2025-06-09 11:34:34 +0300

    start commit

---
+++ TestProjectCI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestProjectCI
+//
+//  Created by Senla on 09.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestProjectCI/TestProjectCIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectCIApp.swift
+//  TestProjectCI
+//
+//  Created by Senla on 09.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectCIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
